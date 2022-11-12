dd88ecab6604f514fe6a46a18556432ecdeff334
nv <nv@nvs-MacBook-Air.local>
2022-11-12 23:00:34 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/belajar_matematika/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vindev.belajar_matematika
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ad_helper.dart
@@ -0,0 +1,9 @@
+class AdHelper {
+  static String get bannerAdUnitId {
+    return 'ca-app-pub-1955915681676898/1670633374';
+  }
+
+  static String get interstitialAd {
+    return 'ca-app-pub-1955915681676898/6156673293';
+  }
+}
